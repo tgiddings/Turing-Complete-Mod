f78e579964ea032c3684fa26ed6f81eaa775201d
Trevor Giddings <tgiddings000@gmail.com>
2025-07-29 22:37:52 -0700

    refactor redstone signal algorithm to be sharable with vanilla redstone and mod blocks, and switchable between the vanilla algorithm and the new algorithm

--- src/main/java/name/turingcomplete/mixin/RedstoneWireBlockMixin.java
+++ src/main/java/name/turingcomplete/mixin/RedstoneWireBlockMixin.java
@@ -1,15 +1,23 @@
 package name.turingcomplete.mixin;
 
 import name.turingcomplete.blocks.ConnectsToRedstone;
+import name.turingcomplete.blocks.logicwire.OnePassWireUpdateStrategy;
 import net.minecraft.block.RedstoneWireBlock;
+
 import org.spongepowered.asm.mixin.Mixin;
 
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
 
 @Mixin(RedstoneWireBlock.class)
 public class RedstoneWireBlockMixin {
@@ -33,5 +41,13 @@ public class RedstoneWireBlockMixin {
         // connect to that side of the Logic Gate.
         e.setReturnValue(logic_block.dustConnectsToThis(state, dir));
         e.cancel();
+    }
+
+    @WrapOperation(
+        method = "neighborUpdate(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/Block;Lnet/minecraft/util/math/BlockPos;Z)V",
+        at = @At(value = "invoke", target = "Lnet/minecraft/block/RedstoneWireBlock;update(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)V")
+    )
+    private void update_neighborUpdate_tcd_mixin(RedstoneWireBlock instance, World world, BlockPos pos, BlockState state, Operation<Void> original, BlockState stateAgain, World worldAgain, BlockPos posAgain, Block sourceBlock, BlockPos sourcePos, boolean notify){
+        new OnePassWireUpdateStrategy().onNeighborUpdate(state, world, pos, sourceBlock, sourcePos, notify);
     }
 }
